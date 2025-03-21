f628cc8b2136e40ff5f5f5f7d10f4f1715d485c7
boranunal <boranutku@protonmail.com>
2025-03-21 22:34:25 +0300

    first part completed 	modified:   web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -3,36 +3,60 @@ from bs4 import BeautifulSoup as bs
 
 # host name, entry point
 base_url = 'https://www.odtuden.com.tr'
-url = base_url + '/kitaplik'
 
-# request the page
-res = requests.get(url)
+# create a global list
+shelf_global = []
 
-# get the content from the respond message
-html = res.content
+def getHTML(url):
+    # request the page
+  res = requests.get(url, allow_redirects=False)
+  if(res.status_code == 200):
+    # get the content from the respond message
+    html = res.content
+    return html
+  else:
+    return 0
 
-# parse html
-soup = bs(html, 'html.parser')
+def getShelfFromPage(html):  
+  # create a list
+  shelf = []
 
-# find product item tag, this is where individual products are,
-# iterate through item_tags to get each item
-item_tag = soup.find('div', class_='productItem')
+  # parse html
+  soup = bs(html, 'html.parser')
 
-# find anchor tag, this is where title and link is found as attributes
-anchor_tag = item_tag.find('a')
+  # find each book on the page
+  item_tags = soup.find_all('div', class_='productItem')
 
-# get title
-title = anchor_tag.get('title').strip()
+  for item_tag in item_tags:
+    # get anchor tag
+    anchor_tag = item_tag.find('a')
+    # get book title
+    title = anchor_tag.get('title').strip()
+    # get book link
+    link = base_url + anchor_tag.get('href').strip()
+    # get book price
+    price = item_tag.find('span', class_='discountPriceSpan').text.strip()
+    # construct a tuple
+    book = (title, link, price)
+    # append the tuple into the list
+    shelf.append(book)
 
-# get link
-link = url + anchor_tag.get('href').strip()
-
-# find the tag where the price is, 
-# get the priceD
-price = item_tag.find('span', class_='discountPriceSpan').text.strip()
-
-print('Title: ', title)
-print('Link: ', link)
-print('Price: ', price)
+  return shelf
 
 
+
+if __name__ == "__main__":
+  page_url = base_url + '/kitaplik?sayfa='
+  for i in range(1,200):
+    html = getHTML(page_url+str(i))
+    if(html==0):
+      break
+    else:
+      shelf_global = shelf_global + (getShelfFromPage(html))
+  
+  k = 1
+  for i in shelf_global:
+    print(str(k)+'.', end='')
+    k = k+1
+    for j in i:
+      print('\t'+ j)
